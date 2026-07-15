002b1426ae2a3e0364f87a4dacd4c4037643f09c
Aryan Massoudy <aryna.massoudy@gmail.com>
2026-07-15 15:03:04 +0200

    working with user input in java

---
+++ day_10/myJavaProject/src/test/java/CalculateStudentGrade.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CalculateStudentGrade {
+    void main () {
+        int totalObtainablePointsExam = 200;
+        int totalObtainablePointsAssignment = 40;
+
+        int studentExamPoints;
+        int studentAssignmentPoints;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter your exam score: ");
+        studentExamPoints = userInput.nextInt();
+
+        System.out.println("Please enter your assignment score: ");
+        studentAssignmentPoints = userInput.nextInt();
+
+        float studentFinalScore = (float) (studentAssignmentPoints + studentExamPoints) / (totalObtainablePointsExam + totalObtainablePointsAssignment);
+        System.out.println("Your final score is : " + studentFinalScore * 100 +"%");
+
+
+    }
+}
